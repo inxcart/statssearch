c5c4403caf3e2d1000532e287b077f8f23a23a08
Michael Dekker <git@michaeldekker.nl>
2017-02-05 07:30:09 +0100

    Update statssearch.php

--- statssearch.php
+++ statssearch.php
@@ -24,7 +24,7 @@
 *  International Registered Trademark & Property of PrestaShop SA
 */
 
-if (!defined('_PS_VERSION_'))
+if (!defined('_TB_VERSION_'))
 	exit;
 
 class StatsSearch extends ModuleGraph
@@ -37,8 +37,8 @@ class StatsSearch extends ModuleGraph
 	{
 		$this->name = 'statssearch';
 		$this->tab = 'analytics_stats';
-		$this->version = '1.4.0';
-		$this->author = 'PrestaShop';
+		$this->version = '2.0.0';
+		$this->author = 'thirty bees';
 		$this->need_instance = 0;
 
 		parent::__construct();
